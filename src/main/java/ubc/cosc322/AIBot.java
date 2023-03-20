bb3b57435b0e0e5e2399fc2b35cd5801b3cf005d
Rithujaa Rajendrakumar <rithujaa128@gmail.com>
2023-03-19 21:40:59 -0700

    Updated main file

--- src/main/java/ubc/cosc322/AIBot.java
+++ src/main/java/ubc/cosc322/AIBot.java
@@ -17,33 +17,30 @@ import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
  *
  */
 public class AIBot extends GamePlayer{
-
+	
+	private final Logger logger;
     private GameClient gameClient = null; 
     private final GameStateManager RosieManager;
     private BaseGameGUI gamegui = null;
     private String username = null;
     private String password = null;
-	private final Logger logger;
  
 	
     /**
      * The main method
      * @param args for name and passwd (current, any string would work)
      */
-    public static void main(String[] args) {
-        AIBot rosie = new AIBot(args[0], args[1]);
-        if (rosie.getGameGUI() == null) {
-            rosie.Go();
-        } else {
-            BaseGameGUI.sys_setup();
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    rosie.Go();
-                }
-            });
-        }
+    public static void main(String[] args) {				 
+    	AIBot rosie = new AIBot(args[0], args[1]);
+    	
+    	if(rosie.getGameGUI() == null) {
+    		rosie.Go();
+    	}
+    	else {
+    		BaseGameGUI.sys_setup();
+            java.awt.EventQueue.invokeLater(rosie::Go);
+    	}
     }
-
 	
     /**
      * Any name and passwd 
@@ -54,12 +51,10 @@ public class AIBot extends GamePlayer{
     	this.username = username;
     	this.password = password;
     	logger = Logger.getLogger(GameStateManager.class.toString());
+    	//To make a GUI-based player, create an instance of BaseGameGUI
+    	//and implement the method getGameGUI() accordingly
+    	this.gamegui = new BaseGameGUI(this);
     	this.RosieManager = new GameStateManager();
-    	if (System.console() == null) {
-            this.gamegui = null;
-        } else {
-            this.gamegui = new BaseGameGUI(this);
-        }
     }
  
 
@@ -95,26 +90,36 @@ public class AIBot extends GamePlayer{
     
     @Override
     public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
-    	if (messageType == GameMessage.GAME_STATE_BOARD) {
-    	    gamegui.setGameState((ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.GAME_STATE));
-    	} else if (messageType == GameMessage.GAME_ACTION_MOVE) {
-    	    gamegui.updateGameState(msgDetails);
-    	    RosieManager.opponentMove(msgDetails);
-    	    moveRosie();
-    	} else if (messageType == GameMessage.GAME_ACTION_START) {
-    	    String black = (String) msgDetails.get(AmazonsGameMessage.PLAYER_BLACK);
-    	    if (black.equalsIgnoreCase(username)) {
-    	        logger.info("Playing as black.");
-    	        RosieManager.setPlayer(GameStateManager.Tile.BLACK);
-    	        moveRosie();
-    	    } else {
-    	        logger.info("Playing as white.");
-    	        RosieManager.setPlayer(GameStateManager.Tile.WHITE);
-    	    }
-    	} else {
-    	    String msg = "Unhandled Message Type occurred: " + messageType;
-    	    logger.warning(msg);
-    	}
+    	switch (messageType) {
+		case GameMessage.GAME_STATE_BOARD -> gamegui.setGameState((ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.GAME_STATE));
+		case GameMessage.GAME_ACTION_MOVE ->  {
+			gamegui.updateGameState(msgDetails);
+			RosieManager.opponentMove(msgDetails);
+			moveRosie();
+
+		}
+		case GameMessage.GAME_ACTION_START -> {
+
+			//Make a move if the bot starts as white
+			String black = (String) msgDetails.get(AmazonsGameMessage.PLAYER_BLACK);
+			if(black.equalsIgnoreCase(username)){
+
+				logger.info("Playing as black.");
+				RosieManager.setPlayer(GameStateManager.Tile.BLACK);
+				moveRosie();
+
+
+			} else {
+				logger.info("Playing as white.");
+				RosieManager.setPlayer(GameStateManager.Tile.WHITE);
+			}
+		}
+		default -> {
+			String msg = "Unhandled Message Type occurred: " + messageType;
+			logger.warning(msg);
+		}
+	}
+
 	return true;
     }
     
