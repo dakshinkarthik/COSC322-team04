0b16450f32032bab93fddd9dbbd81eff1c89f578
andydevarakonda <dvsanirudh@gmail.com>
2023-04-01 20:07:42 -0700

    updated heuristic file

--- src/main/java/ubc/cosc322/Algorithm/Heuristic.java
+++ src/main/java/ubc/cosc322/Algorithm/Heuristic.java
@@ -1,7 +1,11 @@
 package ubc.cosc322.Algorithm;
 
 import ubc.cosc322.Graph.Graph;
+
+
+
 import ubc.cosc322.GameStateManager;
+import ubc.cosc322.GameStateManager.Square;
 
 public class Heuristic {
 
@@ -9,80 +13,82 @@ public class Heuristic {
 
     }
 
-    public static float calculateT(Graph board, GameStateManager.Square turn){
-       
-    	float f1 = 0;                             //calculates the number of filled tiles
-        for(Graph.Node n : board.getNodes()) {
-            if(!n.getValue().isEmpty()) {
-                f1++;
+    public static float calculateHeuristicValue(Graph board, Square player) {
+        float numFilledTiles = 0; //tiles that are not empty
+        for (Graph.Node node : board.getNodes()) {
+            if (!node.getValue().isEmpty()) {
+                numFilledTiles++;
             }
         }
-        f1 = 100 - f1;
-        
-        
-        float f2 = 0;
-        for (Graph.Node n : board.getNodes()) {
-            if (n.getValue().isEmpty()) {
-                continue;
-            }
+        numFilledTiles = 100 - numFilledTiles;
 
-            int qDist = (n.getValue() == GameStateManager.Square.WHITE) ? n.getQdist1() : n.getQdist2();
-            int kDist = (n.getValue() == GameStateManager.Square.WHITE) ? n.getKdist1() : n.getKdist2();
-
-            f2 += Math.pow(qDist, 2) - Math.pow(kDist, 2);
-        }
-        
-        
-        float f3 = 0;                              
-        for (Graph.Node n : board.getNodes()) {
-            if (!n.getValue().isEmpty()) {
-                f3++;
+        float numNonEmptyTiles = 0;
+        for (Graph.Node node : board.getNodes()) {
+            if (!node.getValue().isEmpty()) {
+                numNonEmptyTiles++;
             }
         }
-        f3 = (f3 - 60) / 10;
-        f3 = -(f3 * f3) + 40;
+        numNonEmptyTiles = (numNonEmptyTiles - 30) / 10;
+        numNonEmptyTiles = -(numNonEmptyTiles * numNonEmptyTiles) + 40;
 
-        
-        float f4 = 0;                              //calculates the number of empty tiles
-        f4 = 100 - f1;
-        
+        float numAttackPiecesOnBoard = 0;
+        for (Graph.Node node : board.getNodes()) {
+            if (!node.getValue().isEmpty()) {
+                numAttackPiecesOnBoard++;
+            }
+        }
+        numAttackPiecesOnBoard = (float) ((numAttackPiecesOnBoard - 40));
+        numAttackPiecesOnBoard = -(numAttackPiecesOnBoard * numAttackPiecesOnBoard) + 20;
 
-        double weight = Math.sqrt(f1*f1 + f2*f2 + f3*f3 + f4*f4);
+        float numEmptyTiles = 0;
+        numEmptyTiles = 100 - numFilledTiles;
 
-        float t1 = 0;
-        float t2 = 0;
+        float numArrowPiecesOnBoard = 0;
+        for (Graph.Node node : board.getNodes()) {
+            if (node.getValue().isFire()) {
+                numArrowPiecesOnBoard++;;
+            }
+        }
+        numArrowPiecesOnBoard = (numArrowPiecesOnBoard - 20) / 10;
+        numArrowPiecesOnBoard = (numArrowPiecesOnBoard * numArrowPiecesOnBoard) + 40;
 
-        for (Graph.Node n : board.getNodes()) {
-            if (n.getValue().isEmpty()) {
+        double weight = Math.sqrt(numFilledTiles * numFilledTiles + numNonEmptyTiles * numNonEmptyTiles +
+                numAttackPiecesOnBoard * numAttackPiecesOnBoard + numEmptyTiles * numEmptyTiles + numArrowPiecesOnBoard * numArrowPiecesOnBoard);
+
+        float tiValue1 = 0;
+        float tiValue2 = 0;
+
+        for (Graph.Node node : board.getNodes()) {
+            if (node.getValue().isEmpty()) {
                 continue;
             }
             
-            t1 += Ti_value(turn, n.getQdist1(), n.getQdist2());
-            t2 += Ti_value(turn, n.getKdist1(), n.getKdist2());
+            tiValue1 += calculate_TiValue(player, node.getQdist1(), node.getQdist2());
+            tiValue2 += calculate_TiValue(player, node.getKdist1(), node.getKdist2());
             
         }
 
-        double x1 = (f1/weight) * t1;
-        double x2 = (f2/weight) * C1_value(board);
-        double x3 = (f3/weight) * C2_value(board);
-        double x4 = (f4/weight) * t2;
+        double x1 = (numFilledTiles / weight) * tiValue1;
+        double x2 = (numNonEmptyTiles / weight) * C1_value(board);
+        double x3 = (numAttackPiecesOnBoard / weight) * C2_value(board);
+        double x4 = (numEmptyTiles / weight) * tiValue2;
+        double x5 = (numArrowPiecesOnBoard / weight);
 
-        return (float) (x1 + x2 + x3 + x4);
+        return (float) (x1 + x2 + x3 + x4 + x5);
     }
 
-    private static float Ti_value(GameStateManager.Square player, int dist1, int dist2){
-    	float k = 1/5f;
+    private static float calculate_TiValue(GameStateManager.Square playerSquare, int dist1, int dist2){
+    	float k_constant = 1/4f;
 
-        if(dist1 == Integer.MAX_VALUE && dist2 == Integer.MAX_VALUE) 
-            return 0;
-        else if(dist1 == dist2) {
-            if (player ==  GameStateManager.Square.BLACK)
-                return -k;
-            else
-                return k;  
-        }  
-        else 
-            return dist1 < dist2 ? 1 : -1;
+       
+    
+        if(dist1 == Integer.MAX_VALUE && dist2 == Integer.MAX_VALUE) return 0;
+       
+        else if(dist1 == dist2) return playerSquare == GameStateManager.Square.WHITE ? k_constant : -k_constant;
+        
+        else if(dist1 < dist2) return 1;
+       
+        else return -1;
     }
 
     private static float C1_value(Graph board){
@@ -103,6 +109,11 @@ public class Heuristic {
 
         return sum;
     }
+    
+    }
+    
+    
+    
 
 
-}
+
